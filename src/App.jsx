1fc6a5c17d0d879af50b6e6bb41cb0aa69b52d1b
chanchal16 <rajputchanchal17@gmail.com>
2022-08-29 15:50:25 +0530

    Update - add cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
+import { Cart } from './pages/Cart'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+      <Cart/>
     </div>
   )
 }
