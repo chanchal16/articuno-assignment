00bef14354c87c02822cc955c90826dfb714b573
chanchal16 <rajputchanchal17@gmail.com>
2022-08-30 20:55:43 +0530

    Update - add Payment method & cart items component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { CartItems } from '../components/CartItems'
 import { Form } from '../components/Form'
+import { PaymentMethod } from '../components/PaymentMethod'
 
 export const Cart = () => {
   return (
     <div>
+      <div className='flex p-2 gap-4'>
         <Form/>
+        <PaymentMethod/>
+        <CartItems/>
+      </div>
     </div>
   )
 }
