c6413096036ab78560d41fc08934b2333a3d2c5e
chanchal16 <rajputchanchal17@gmail.com>
2022-08-31 10:29:50 +0530

    Update - add user state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Cart } from './pages/Cart'
 
 function App() {
+  const initialValue = {
+    name:'',email:'',password:''
+  }
+  const [userData,setUserData] = useState(JSON.parse(localStorage.getItem('user')) || initialValue)
 
   return (
     <div className="App">
-      <Navbar/>
-      <Cart/>
+      <Navbar user={userData}/>
+      <Cart userData={userData} setUserData={setUserData}/>
     </div>
   )
 }
